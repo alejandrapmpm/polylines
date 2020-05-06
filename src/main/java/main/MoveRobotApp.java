8dbb7600a46c80d4d2718afcd88c716bf76c3c00
Alejandra Porras <alejandra.porras@sky.uk>
2020-05-06 12:58:22 +0100

    Generate Jar executable and receive polyline from argument

--- src/main/java/main/MoveRobotApp.java
+++ src/main/java/main/MoveRobotApp.java
@@ -21,13 +21,7 @@ public class MoveRobotApp {
     private static JsonReportPrinter jsonReportPrinter = new JsonReportPrinter();
 
     public static void main(String[] args) throws RobotValidationException {
-        String polyline = "mpjyHx`i@VjAVKnAh@BHHX@LZR@Bj@Ml@WWc@]w@bAyAfBmCb@o@pLeQfCsDVa@@ODQR}AJ{A?{BGuAD_@FKb@MTUX]Le@" +
-                "^kBVcAVo@Ta@|EaFh@m@FWaA{DCo@q@mCm@cC{A_GWeA}@sGSeAcA_EOSMa@}A_GsAwFkAiEoAaFaBoEGo@]_AIWW{AQyAUyBQqAI_BF" +
-                "kEd@aHZcDlAyJLaBPqDDeD?mBEiA}@F]yKWqGSkICmCIeZIuZi@_Sw@{WgAoXS{DOcAWq@KQGIFQDGn@Y`@MJEFIHyAVQVOJGHgFRJBB" +
-                "CCSKBcAKoACyA?m@^yVJmLJ{FGGWq@e@eBIe@Ei@?q@Bk@Hs@Le@Rk@gCuIkJcZsDwLd@g@Oe@o@mB{BgHQYq@qBQYOMSMGBUBGCYc@E" +
-                "_@H]DWJST?JFFHBDNBJ?LED?LBv@WfAc@@EDGNK|@e@hAa@`Bk@b@OEk@Go@IeACoA@a@PyB`@yDDc@e@K{Bi@oA_@w@]m@_@]QkBoAw" +
-                "C{BmAeAo@s@uAoB_AaBmAwCa@mAo@iCgAwFg@iDq@}G[uEU_GBuP@cICmA?eI?qCB{FBkCI}BOyCMiAGcAC{AN{YFqD^}FR}CNu@JcAH" +
-                "u@b@_E`@}DVsB^mBTsAQKkCmAg@[YQOIOvAi@[m@e@s@g@GKCKAEJIn@g@GYGIc@ScBoAf@{A`@uAlBfAG`@";
+        String polyline = args[0];
 
         List<GeoPoint> journey = generateGeoPoints(new EncodedPolyline(polyline));
 
